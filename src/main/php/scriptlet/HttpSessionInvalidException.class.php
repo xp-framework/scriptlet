c1a9202b1a6d13beaec8398d413d7765f5b9c31f
No author <xp-framework-null@xp-framework.de>
1999-08-19 14:09:20 +0000

    New repository initialized by cvs2svn.
    git-svn-id: svn://svn.xp-framework.net/xp/trunk@1 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ src/main/php/scriptlet/HttpSessionInvalidException.class.php
@@ -0,0 +1,17 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('scriptlet.ScriptletException');
+
+  /**
+   * Indicates the session is invalid
+   *
+   * @see      xp://scriptlet.ScriptletException
+   * @purpose  Exception
+   */  
+  class HttpSessionInvalidException extends ScriptletException {
+  }
+?>
