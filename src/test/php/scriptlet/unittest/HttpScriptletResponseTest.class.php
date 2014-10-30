6e17a1a3ef78dae039f6f7ce63632fee1d062100
Timm Friebe <thekid@thekid.de>
2014-10-30 22:32:40 +0100

    QA: Adjust to new coding standards

--- src/test/php/scriptlet/unittest/HttpScriptletResponseTest.class.php
+++ src/test/php/scriptlet/unittest/HttpScriptletResponseTest.class.php
@@ -3,7 +3,6 @@
 use unittest\TestCase;
 use scriptlet\HttpScriptletResponse;
 
-
 /**
  * TestCase
  *
@@ -14,35 +13,22 @@ class HttpScriptletResponseTest extends TestCase {
 
   /**
    * Set up this testcase
-   *
    */
   public function setUp() {
     $this->r= new HttpScriptletResponse();
   }
 
-  /**
-   * Test no headers are set initially
-   *
-   */
   #[@test]
   public function noHeaders() {
     $this->assertEquals([], $this->r->headers);
   }
 
-  /**
-   * Test adding headers works
-   *
-   */
   #[@test]
   public function addHeader() {
     $this->r->setHeader('header', 'value');
     $this->assertEquals('value', $this->r->getHeader('header'));
   }
 
-  /**
-   * Test adding headers twice works
-   *
-   */
   #[@test]
   public function addHeaderTwice() {
     $this->r->setHeader('header', 'value');
@@ -51,39 +37,23 @@ class HttpScriptletResponseTest extends TestCase {
     $this->assertEquals(2, sizeof($this->r->headers));
   }
 
-  /**
-   * Test
-   *
-   */
   #[@test]
   public function lookupCaseInsensitive() {
     $this->r->setHeader('header', 'value');
     $this->assertEquals('value', $this->r->getHeader('HEADER'));
   }
 
-  /**
-   * Test
-   *
-   */
   #[@test]
   public function nonexistingHeaderReturnsDefault() {
     $this->assertEquals('default', $this->r->getHeader('does_not_exist', 'default'));
   }
 
-  /**
-   * Test
-   *
-   */
   #[@test]
   public function writeToOutputStream() {
     $this->r->getOutputStream()->write('Hello');
     $this->assertEquals('Hello', $this->r->getContent());
   }
 
-  /**
-   * Test sendContent()
-   *
-   */
   #[@test]
   public function sendContent() {
     $this->r->setContent('Test');
@@ -94,10 +64,6 @@ class HttpScriptletResponseTest extends TestCase {
     $this->assertEquals('Test', $content);
   }
 
-  /**
-   * Test sendContent()
-   *
-   */
   #[@test]
   public function doNotSendNullContent() {
     $this->r->setContent(null);
@@ -108,58 +74,34 @@ class HttpScriptletResponseTest extends TestCase {
     $this->assertEquals('', $content);
   }
 
-  /**
-   * Test flush()
-   *
-   */
   #[@test]
   public function flush() {
     $this->r->flush();
   }
 
-  /**
-   * Test flush()
-   *
-   */
   #[@test, @expect('lang.IllegalStateException')]
   public function flushCalledTwice() {
     $this->r->flush();
     $this->r->flush();
   }
 
-  /**
-   * Test isCommitted()
-   *
-   */
   #[@test]
   public function isCommitted() {
     $this->assertFalse($this->r->isCommitted());
   }
 
-  /**
-   * Test isCommitted()
-   *
-   */
   #[@test]
   public function isCommittedAfterFlush() {
     $this->r->flush();
     $this->assertTrue($this->r->isCommitted());
   }
 
-  /**
-   * Test write()
-   *
-   */
   #[@test]
   public function writeToBuffer() {
     $this->r->write('Hello');
     $this->assertEquals('Hello', $this->r->getContent());
   }
 
-  /**
-   * Test write()
-   *
-   */
   #[@test]
   public function writeDirectly() {
     $this->r->flush();
@@ -172,10 +114,6 @@ class HttpScriptletResponseTest extends TestCase {
     $this->assertEquals('Hello', $content);
   }
 
-  /**
-   * Test write()
-   *
-   */
   #[@test]
   public function writeBufferedAndDirectWrites() {
     $this->r->write('Hello');   // This will be buffered
