f506accca9e647dced68a04680f72f9d6ed8cb67
Timm Friebe <thekid@thekid.de>
2014-12-14 14:43:28 +0100

    Verify not calling proceed() will stop chain

--- src/test/php/scriptlet/unittest/FilterTest.class.php
+++ src/test/php/scriptlet/unittest/FilterTest.class.php
@@ -6,8 +6,7 @@ use scriptlet\HttpScriptletRequest;
 use scriptlet\HttpScriptletResponse;
 use scriptlet\ScriptletException;
 use peer\URL;
-use peer\http\HttpConstants;
-use lang\IllegalAccessException;
+use lang\IllegalStateException;
 
 /**
  * TestCase
@@ -64,5 +63,27 @@ class FilterTest extends ScriptletTestCase {
 
     $this->assertEquals(['Filtered: one', 'Filtered: two'], $res->headers);
     $this->assertEquals('Invoked', $res->content);
+  }
+
+  #[@test]
+  public function do_not_proceed() {
+    $req= $this->newRequest('GET', new URL('http://localhost'));
+    $res= new HttpScriptletResponse();
+
+    $s= newinstance('scriptlet.HttpScriptlet', [], [
+      'doGet' => function($request, $response) {
+        throw new IllegalStateException('Will not be reached');
+      }
+    ]);
+    $s->filter(newinstance('scriptlet.Filter', [], [
+      'filter' => function($request, $response, $invocation) {
+        $response->setHeader('Filtered', 'true');
+        return;
+      }
+    ]));
+    $s->service($req, $res);
+
+    $this->assertEquals(['Filtered: true'], $res->headers);
+    $this->assertEquals(null, $res->content);
   }
 }
