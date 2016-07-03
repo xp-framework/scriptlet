f954aaadb3923512876cf2e6f8cf94cab6309e97
Timm Friebe <thekid@thekid.de>
2016-07-03 17:14:41 +0200

    For old runners, pass config directories inside `-c`

--- src/main/php/xp/scriptlet/Server.class.php
+++ src/main/php/xp/scriptlet/Server.class.php
@@ -9,7 +9,7 @@ class Server {
    * Entry point method. Receives the following arguments from xpws:
    *
    * 0. The web root - defaults to $CWD
-   * 1. The application source - either a directory or ":" + f.q.c.Name
+   * 1. The application source - either a directory or a layout or scriptlet class name
    * 2. The server profile - default to "dev"
    * 3. The server address - default to "localhost:8080"
    * 4. The mode - default to "serve" (can include further arguments to the server constructor separated by commas)
@@ -18,13 +18,24 @@ class Server {
    * @return  int
    */
   public static function main(array $args) {
-    WebRunner::main([
+    $args= [
       '-r', isset($args[0]) ? realpath($args[0]) : getcwd(),
       '-p', isset($args[2]) ? $args[2] : 'dev',
       '-a', isset($args[3]) ? $args[3] : 'localhost:8080',
-      '-m', isset($args[4]) ? $args[4] : 'serve',
-      isset($args[1]) ? $args[1] : 'etc'
-    ]);
+      '-m', isset($args[4]) ? $args[4] : 'serve'
+    ];
+
+    if (isset($args[1])) {
+      $sources= explode(PATH_SEPARATOR, ltrim($args[1], ':'));
+      $source= array_shift($sources);
+      foreach ($sources as $dir) {
+        $args[]= '-c';
+        $args[]= $dir;
+      }
+      $args[]= $source;
+    }
+
+    WebRunner::main($args);
     return 0;
   }
 }
