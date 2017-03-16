36c6c69d9039eb8cd2474e593cf3777b45bf617d
johannes85 <johannes@johnimedia.de>
2017-03-16 10:39:24 +0000

    Moved chain call to position outside of the error handling.

--- src/main/php/scriptlet/AuthenticationFilter.class.php
+++ src/main/php/scriptlet/AuthenticationFilter.class.php
@@ -34,11 +34,11 @@ class AuthenticationFilter extends \lang\Object implements Filter {
   public function filter($request, $response, $invocation) {
     try {
       $r= $this->auth->authenticate($request, $response, null);
-      return false === $r ? $r : $invocation->proceed($request, $response);
     } catch (ScriptletException $e) {
       throw $e;
     } catch (Throwable $e) {
       throw new ScriptletException('Authentication failed: '.$e->getMessage(), HttpConstants::STATUS_FORBIDDEN, $e);
     }
+    return false === $r ? $r : $invocation->proceed($request, $response);
   }
 }
