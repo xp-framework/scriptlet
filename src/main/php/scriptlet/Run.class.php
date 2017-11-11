41a433a7b65953fbc31f4b6a5778af8bd37c6040
Timm Friebe <thekid@thekid.de>
2017-11-11 17:03:12 +0100

    Adjust to xp-forge/web using util.URI

--- src/main/php/scriptlet/Run.class.php
+++ src/main/php/scriptlet/Run.class.php
@@ -1,5 +1,9 @@
 <?php namespace scriptlet;
 
+use web\InternalServerError;
+use web\Error;
+use lang\Throwable;
+
 /**
  * Adapter class for new web API
  *
@@ -17,19 +21,25 @@ class Run implements \web\Handler {
     $this->scriptlet->init();
   }
 
+  /**
+   * Handler
+   *
+   * @param  web.Request $request
+   * @param  web.Response $response
+   */
   public function handle($request, $response) {
     with ($req= $this->scriptlet->request(), $res= $this->scriptlet->response()); {
       $uri= $request->uri();
-      $port= $uri->getPort(-1);
+      $port= $uri->port() ?: -1;
 
       // Proxy request
-      $req->url= new HttpScriptletURL($uri->getURL());
+      $req->url= new HttpScriptletURL($uri->asString(true));
       $req->method= $request->method();
       $req->env= [
         'SERVER_PROTOCOL' => 'HTTP/1.1',
-        'REQUEST_URI'     => $uri->getPath(),
-        'QUERY_STRING'    => $uri->getQuery(),
-        'HTTP_HOST'       => $uri->getHost().(-1 === $port ? '' : ':'.$port)
+        'REQUEST_URI'     => $uri->path(true),
+        'QUERY_STRING'    => $uri->query(true),
+        'HTTP_HOST'       => $uri->host().(-1 === $port ? '' : ':'.$port)
       ];
       $req->setParams($request->params());
       $req->setHeaders(array_merge($request->headers(), $request->values()));
@@ -38,26 +48,36 @@ class Run implements \web\Handler {
       };
 
       // Proxy response
-      $res->sendHeaders= function($version, $statusCode, $headers) use($response) {
+      $stream= null;
+      $res->sendHeaders= function($version, $statusCode, $headers) use($response, &$stream) {
         $response->answer($statusCode);
         foreach ($headers as $header) {
           sscanf($header, "%[^:]: %[^\r]", $name, $value);
+          if (0 === strcasecmp('Content-Length', $name)) {
+            $stream= $response->stream($value);
+          }
           $response->header($name, $value);
         }
+
+        // Fall back to chunked encoding
+        if (null === $stream) $stream= $response->stream(null);
       };
-      $res->sendContent= function($content) use($response) {
-        $response->write($content);
+      $res->sendContent= function($content) use(&$stream) {
+        $stream->write($content);
       };
 
       // Run scriptlet
       try {
         $this->scriptlet->service($req, $res);
       } catch (ScriptletException $e) {
-        throw new InternalServerError($e);
+        throw new Error($e->getStatus(), $e->getMessage(), $e);
+      } catch (Throwable $t) {
+        throw new InternalServerError($t);
       }
 
       $res->isCommitted() || $res->flush();
       $res->sendContent();
+      $stream->close();
     }
   }
 }
